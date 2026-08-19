c2a18d226b911ac5008f5e47022249cb3670320d
sarva12 <130266747+sarva12@users.noreply.github.com>
2026-08-19 01:51:01 -0400

    Add multi-alarm persistence model [skip ci]

---
+++ CatchUp/Models/CatchUpAlarm.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+enum AlarmDeliveryMode: String, Codable, Sendable {
+    case alarmKit
+    case notification
+}
+
+struct CatchUpAlarm: Identifiable, Codable, Equatable, Sendable {
+    let id: UUID
+    var time: Date
+    var label: String
+    var isEnabled: Bool
+    var systemID: UUID?
+    var deliveryMode: AlarmDeliveryMode?
+
+    init(
+        id: UUID = UUID(),
+        time: Date,
+        label: String = "Morning catch-up",
+        isEnabled: Bool = true,
+        systemID: UUID? = nil,
+        deliveryMode: AlarmDeliveryMode? = nil
+    ) {
+        self.id = id
+        self.time = time
+        self.label = label
+        self.isEnabled = isEnabled
+        self.systemID = systemID
+        self.deliveryMode = deliveryMode
+    }
+}
+
+struct ScheduledAlarm: Sendable {
+    let systemID: UUID
+    let deliveryMode: AlarmDeliveryMode
+}
+
