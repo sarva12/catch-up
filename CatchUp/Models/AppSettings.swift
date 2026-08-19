7dee206aa77f5ce858ea551455076c00f39fa9dd
sarva12 <130266747+sarva12@users.noreply.github.com>
2026-08-19 02:03:54 -0400

    Finalize four-story flow in AppSettings.swift [skip ci]

--- CatchUp/Models/AppSettings.swift
+++ CatchUp/Models/AppSettings.swift
@@ -12,10 +12,9 @@ enum NewsTopic: String, CaseIterable, Codable, Identifiable, Sendable {
 
 struct AppSettings: Codable, Sendable {
     var selectedTopics: Set<NewsTopic> = [.world, .energy, .technology]
-    var dailyStoryCount = 3
+    var dailyStoryCount = 4
     var backendURL = ""
     var onboardingCompleted = false
     var shieldingEnabled = false
 }
-
 
